a829117a85cee8834debeb6a8785782b82980dea
nevster08 <nevster07@gmail.com>
2015-11-15 18:59:25 +0000

    Changed location

--- src/GraphicsAssignment2D/main.cpp
+++ src/GraphicsAssignment2D/main.cpp
@@ -451,7 +451,7 @@ void initializeTextures()
 	unsigned char* image = SOIL_load_image("img.jpg", &width, &height, 0, SOIL_LOAD_RGB);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
 	SOIL_free_image_data(image);
-	glUniform1f(glGetUniformLocation(theProgram, "theTex"), 0);
+	glUniform1f(glGetUniformLocation(theProgram, "tex"), 0);
 	
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
